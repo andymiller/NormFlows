3697cd8b6345cb9fd6bda3f6852cb839216f64e8
Andrew Miller <andrew.colin.miller@gmail.com>
2017-03-20 22:28:51 -0400

    simple setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Main module target
+setup(name='normflows',
+      author="Andrew Miller",
+      license="MIT",
+      version='0.0.0',
+      packages=['normflows'],
+      ext_modules=[]
+     )
